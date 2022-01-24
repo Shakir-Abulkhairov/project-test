613f68c5186133ee2bed14c583f67a8d5d5ab22e
Shakir <shakirabulkhairov@gmail.com>
2022-01-24 19:12:32 +0600

    first change on my project

---
+++ index.js
@@ -0,0 +1,15 @@
+let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?','');
+let personalMovieDB = {
+    count:numberOfFilms,
+    movies:{},
+    actors:{},
+    genres:[],
+    privat:false
+}
+
+let a = prompt('Один из последних просмотренных фильмов?',''),
+    b = prompt('На сколько оцените его?','');
+
+    personalMovieDB.movies[a] = b; 
+
+console.log(personalMovieDB)
